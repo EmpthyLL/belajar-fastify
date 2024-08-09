eb2703a9ed26641bc01bfb39aa5f9bc4e1bcceb4
howard <javileh40@gmail.com>
2024-08-09 16:54:23 +0700

    MySQL

--- books.sql
+++ books.sql
@@ -1,7 +1,17 @@
 create database bookstore;
 
+CREATE TABLE `bookstore`.`books` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `author` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`)
+);
+
+
 insert into books value 
 (3,"Laskar Pelangi","Andrea Hirata"),
 (4,"Filosofi Kopi","Dewi Lestari"),
 (1,"Nanti Kita Cerita tentang Hari Ini","Marchella FP"),
-(2,"Rudy: Kisah Masa Muda Sang Visioner","Gina S. Noer")
+(2,"Rudy: Kisah Masa Muda Sang Visioner","Gina S. Noer");
+
+delete from books where title = "Madilog";
